54b2ccd1eba9dba82475bb901144326ec6150b82
Eduardo Gomes <edugomes514@gmail.com>
2023-08-29 08:18:27 -0300

    Questao 8 com criação de classe 'produto'

--- Lista2/Questao8/src/Produto.java
+++ Lista2/Questao8/src/Produto.java
@@ -3,17 +3,16 @@ public class Produto {
     private String nome;
     private double preco;
 
-    public Produto(String nome, double preco) {
+    public Produto(String nome, double preco) {// Construtor da classe Produto
         this.nome = nome;
         this.preco = preco;
     }
 
-    public double getPreco() {
+    public double getPreco() { // Método getPreco para retornar o preço do produto
         return this.preco;
     }
 
-
-    public String toString() {
+    public String toString() { // Método toString para imprimir o produto
         return this.nome + " - R$ " + this.preco;
     }
 
